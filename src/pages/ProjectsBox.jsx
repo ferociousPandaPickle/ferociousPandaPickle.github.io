bda7f59cf35482ad078e659a06060db00cee67e3
ferociousPandaPickle <pranavkadiyam777@gmail.com>
2025-06-23 15:04:40 +0700

    works on mobile now

--- src/pages/ProjectsBox.jsx
+++ src/pages/ProjectsBox.jsx
@@ -6,31 +6,33 @@ function Projects(){
     return(
         <>
         <div className="pages">
-            <div className="project-card">
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
-                    <img src= {personalweb} width={400} height={200} alt="skill-cards" className="project-photo"></img>
-                </a>
-                <h2>Personal Portifolio V-1</h2>
-                <p>This is my personal portifolio</p>
-                <div>
-                    <button>React</button>
-                    <button>CSS</button>
-                    <button>HTML</button>
-                </div>
-            </div>
 
-            <div className="project-card">
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
-                    <img src= {personalweb} width={400} height={200} alt="skill-cards" className="project-photo"></img>
-                </a>                <h2>Personal Portifolio V-1</h2>
-                <p>This is my personal portifolio</p>
-                <div>
-                    <button>React</button>
-                    <button>CSS</button>
-                    <button>HTML</button>
+            <div className="project-container">
+                <div className="project-card">
+                    <a href="https://github.com/ferociousPandaPickle/ferociousPandaPickle.github.io" target="_blank" rel="noopener noreferrer">
+                        <img src= {personalweb} alt="skill-cards" className="project-photo"></img>
+                    </a>
+                    <h2>Personal Portifolio V-1</h2>
+                    <p>This is my personal portifolio</p>
+                    <div className="tech-tags">
+                        <span>React</span>
+                        <span>CSS</span>
+                        <span>HTML</span>
+                    </div>
                 </div>
+                {/* <div className="project-card">
+                    <a href="https://github.com/ferociousPandaPickle/ferociousPandaPickle.github.io" target="_blank" rel="noopener noreferrer">
+                        <img src= {personalweb} alt="skill-cards" className="project-photo"></img>
+                    </a>
+                    <h2>Personal Portifolio V-1</h2>
+                    <p>This is my personal portifolio</p>
+                    <div className="tech-tags">
+                        <span>React</span>
+                        <span>CSS</span>
+                        <span>HTML</span>
+                    </div>
+                </div> */}
             </div>
-
         </div>
         </>
     )
