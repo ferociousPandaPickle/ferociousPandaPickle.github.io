4559af69596de86f72878ed2353a4cab67e81ea7
ferociousPandaPickle <pranavkadiyam777@gmail.com>
2025-08-20 15:35:19 -0700

    resume in public now

--- src/pages/HomeBox.jsx
+++ src/pages/HomeBox.jsx
@@ -31,7 +31,7 @@ function Home(){
 
             <div className="bottomButtons">
                 <a
-                href="personal-portifolio\src\assets\Internship Resume.pdf"
+                href="personal-portifolio\public\Internship Resume.pdf"
                 target='_blank'
                 download="Pranav-Kadiyam-Resume.pdf"
                 style={{ textDecoration: 'none',   color: '#00f0ff', }}
